c1a16447d85e20a052456cae8e45b0be38a17191
ciran541 <ciranjivigokul@gmail.com>
2024-10-20 11:43:54 +0530

    blog url change

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -13,7 +13,7 @@ const Blog = () => {
        <div className="flex flex-wrap justify-center gap-5">
         {/* card 1 */}
        <div className=" w-full lg:w-1/4 p-2 shadow-2xl space-y-2 rounded-lg cursor-pointer hover:scale-105 transition duration-300 ease-in-out">
-            <img className='h-64 md:h-96 lg:h-40 w-full rounded-lg' src="src/assets/blog-1.avif" alt="img" />
+            <img className='h-64 md:h-96 lg:h-40 w-full rounded-lg' src="assets/blog-1.avif" alt="img" />
             <h1 className="text-lg text-center font-semibold">Exoplanet Discovery and Characterization Software</h1>
             <p className="text-center text-sm"> Techbrain Networks specializes in satellite software development, mission control systems, data analysis for space missions, and solutions for satellite communication.</p>
         </div>
@@ -21,14 +21,14 @@ const Blog = () => {
            {/* card 2 */}
             
            <div className=" w-full lg:w-1/4 p-2 shadow-2xl space-y-2 rounded-lg cursor-pointer hover:scale-105 transition duration-300 ease-in-out">
-            <img className='h-64 md:h-96 lg:h-40 w-full rounded-lg' src="src/assets/blog-2.avif" alt="img" />
+            <img className='h-64 md:h-96 lg:h-40 w-full rounded-lg' src="assets/blog-2.avif" alt="img" />
             <h1 className="text-lg text-center font-semibold">Planetary GIS Mapping Applications</h1>
             <p className="text-center text-sm"> Gain detailed insights into the geology and topography of celestial bodies, supporting planetary exploration and research.</p>
         </div>
 
         {/* card 3 */}
         <div className=" w-full lg:w-1/4 p-2 shadow-2xl space-y-2 rounded-lg cursor-pointer hover:scale-105 transition duration-300 ease-in-out">
-            <img className='h-64 md:h-96 lg:h-40 w-full rounded-lg' src="src/assets/blog-3.avif" alt="img" />
+            <img className='h-64 md:h-96 lg:h-40 w-full rounded-lg' src="assets/blog-3.avif" alt="img" />
             <h1 className="text-lg text-center font-semibold">Orbital Maneuvering Optimization Software</h1>
             <p className="text-center text-sm"> Techbrain Networks actively contributes to space exploration by developing advanced software solutions that enhance communication, data analysis, and overall efficiency in space missions.</p>
         </div>
@@ -36,14 +36,14 @@ const Blog = () => {
         {/* card 4 */}
 
         <div className=" w-full lg:w-1/4 p-2 shadow-2xl space-y-2 rounded-lg cursor-pointer hover:scale-105 transition duration-300 ease-in-out">
-            <img className='h-64 md:h-96 lg:h-40 w-full rounded-lg' src="src/assets/blog-4.avif" alt="img" />
+            <img className='h-64 md:h-96 lg:h-40 w-full rounded-lg' src="assets/blog-4.avif" alt="img" />
             <h1 className="text-lg text-center font-semibold">Microgravity Research Simulation Tools</h1>
             <p className="text-center text-sm"> Techbrain Networks offers services such as satellite software development, mission control systems, data analysis for space missions, and solutions for satellite communication.</p>
         </div>
          {/* card 5 */}
 
          <div className=" w-full lg:w-1/4 p-2 shadow-2xl space-y-2 rounded-lg cursor-pointer hover:scale-105 transition duration-300 ease-in-out">
-            <img className='h-64 md:h-96 lg:h-40 w-full rounded-lg' src="src/assets/blog-5.avif" alt="img" />
+            <img className='h-64 md:h-96 lg:h-40 w-full rounded-lg' src="assets/blog-5.avif" alt="img" />
             <h1 className="text-lg text-center font-semibold">Space-based Earth Observation Platforms</h1>
             <p className="text-center text-sm">Our team actively participates in industry events, collaborates with experts, and engages in ongoing research to stay updated with the latest trends and innovations in space technology.</p>
         </div>
@@ -51,7 +51,7 @@ const Blog = () => {
          {/* card 6 */}
           
          <div className=" w-full lg:w-1/4 p-2 shadow-2xl space-y-2 rounded-lg cursor-pointer hover:scale-105 transition duration-300 ease-in-out">
-            <img className='h-64 md:h-96 lg:h-40 w-full rounded-lg' src="src/assets/blog-6.avif" alt="img" />
+            <img className='h-64 md:h-96 lg:h-40 w-full rounded-lg' src="assets/blog-6.avif" alt="img" />
             <h1 className="text-lg text-center font-semibold">Space Weather Forecasting Software</h1>
             <p className="text-center text-sm">Absolutely. Techbrain Networks specializes in developing solutions for satellite communication, ensuring reliable and efficient communication in space-related initiatives.</p>
         </div>
